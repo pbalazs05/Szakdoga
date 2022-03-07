e7979c4a2caeaa64b94fff827409ed9a8eee4584
Pál <palbalazs98@outlook.com>
2022-03-07 21:33:58 +0100

    backend app.js szépités

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){ //ha a környezet nem lenne töltenne be
+if (process.env.NODE_ENV !== 'production') { //ha a környezet nem lenne töltenne be
     require('dotenv').config()
 }
 
@@ -47,62 +47,62 @@ const GetAllUserwithPendingSubject = require('./routes/Pending');
 const GetEditSemesterRoute = require('./routes/EditSemester');
 const GetExamBoard = require('./routes/ExamBoard');
 
-var cors= require('cors');
+var cors = require('cors');
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
 }))
 
 app.use('/api/posts', postsRoute);
 app.use('/api/subjects', SubjectRoute);
 app.use('/api/NameAndEmail', NameAndEmailRoute);
-app.use('/api/user-login',UserLoginRoute);
-app.use('/api/adminlogin',AdminLoginRoute);
+app.use('/api/user-login', UserLoginRoute);
+app.use('/api/adminlogin', AdminLoginRoute);
 app.use('/api/RegUser', UserRegRoute);
 app.use('/api/RegAdmin', AdminRegRoute);
 app.use('/api/getsubjects', GetSubjectsRoute);
 app.use('/api/getdoctoralprograms', GetDoctoralProgramsRoute);
 app.use('/api/getteachers', GetTeachersRoute);
-app.use('/api/getusers',GetUsersRoute);
-app.use('/api/getadmins',GetAdminsRoute);
-app.use('/api/changeadminpass',GetAdminChangePassRoute);
-app.use('/api/changeuserpass',GetUserChangePassRoute);
-app.use('/api/editteachers',GetEditTeachersRoute);
-app.use('/api/editdoctoralprograms',GetEditDoctoralProgramsRoute);
-app.use('/api/editsubjects',GetEditSubjectsRoute);
-app.use('/api/editadmins',GetEditAdminsRoute);
-app.use('/api/editusers',GetEditUsersRoute);
-app.use('/api/deleteteachers',GetDeleteTeachersRoute);
-app.use('/api/deleteprograms',GetDeleteProgramsRoute);
-app.use('/api/deletesubjects',GetDeleteSubjectsRoute);
-app.use('/api/deleteadmins',GetDeleteAdminsRoute);
-app.use('/api/deleteusers',GetDeleteUsersRoute);
-app.use('/api/gennewpassforusers',GetGenNewUserPassRoute);
-app.use('/api/gennewpassforadmin',GetGenNewAdminPassRoute);
-app.use('/api/PostAdministrate',GetAdministratePostRoute);
-app.use('/api/subjectupdate',GetSubjectUpdateRoute);
-app.use('/api/getpendingusers',GetAllUserwithPendingSubject);
-app.use('/api/editsemester',GetEditSemesterRoute);
-app.use('/api/examboard',GetExamBoard);
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true,useUnifiedTopology: true });
+app.use('/api/getusers', GetUsersRoute);
+app.use('/api/getadmins', GetAdminsRoute);
+app.use('/api/changeadminpass', GetAdminChangePassRoute);
+app.use('/api/changeuserpass', GetUserChangePassRoute);
+app.use('/api/editteachers', GetEditTeachersRoute);
+app.use('/api/editdoctoralprograms', GetEditDoctoralProgramsRoute);
+app.use('/api/editsubjects', GetEditSubjectsRoute);
+app.use('/api/editadmins', GetEditAdminsRoute);
+app.use('/api/editusers', GetEditUsersRoute);
+app.use('/api/deleteteachers', GetDeleteTeachersRoute);
+app.use('/api/deleteprograms', GetDeleteProgramsRoute);
+app.use('/api/deletesubjects', GetDeleteSubjectsRoute);
+app.use('/api/deleteadmins', GetDeleteAdminsRoute);
+app.use('/api/deleteusers', GetDeleteUsersRoute);
+app.use('/api/gennewpassforusers', GetGenNewUserPassRoute);
+app.use('/api/gennewpassforadmin', GetGenNewAdminPassRoute);
+app.use('/api/PostAdministrate', GetAdministratePostRoute);
+app.use('/api/subjectupdate', GetSubjectUpdateRoute);
+app.use('/api/getpendingusers', GetAllUserwithPendingSubject);
+app.use('/api/editsemester', GetEditSemesterRoute);
+app.use('/api/examboard', GetExamBoard);
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection
-db.on('error',error => console.error(error));
-db.once('open',()=>console.log('connected'))
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('connected'))
 //Starting listening
 //app.listen(50111);
 
 //https-hez
-app.get('/',(reg, res, next)=>{
+app.get('/', (reg, res, next) => {
     res.status(200);
-    res.setHeader('Acces-Control-Allow-Origin','*');
-    res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
+    res.setHeader('Acces-Control-Allow-Origin', '*');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Acces-Contorl-Allow-Methods', 'Content-Type', 'Authorization');
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 
@@ -112,6 +112,6 @@ const https = require('https');
 const server = https.createServer({ key, cert }, app);
 const port = 50111;
 server.listen(port, () => {
-  console.log(`Server is listening on https://localhost:${port}`);
+    console.log(`Server is listening on https://localhost:${port}`);
 });
 //
